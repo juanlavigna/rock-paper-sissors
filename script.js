394cbb0ab3d0657339731579b2c290bb4efe9810
Juan Lavigna <juanlavigna@gmail.com>
2022-05-28 00:29:53 -0300

    Crea la funcion jugada que recrea una jugada

--- script.js
+++ script.js
@@ -2,8 +2,29 @@ let armas = ["piedra", "papel", "tijera"]
 
 function computerPlay (){
     let computerChoice = armas[Math.floor(Math.random()*armas.length)]
-    console.log(`La computadora eligió ${computerChoice}.`) 
+    return computerChoice
 } 
 
-computerPlay()
+
+function jugada (computer, user) {
+    if (      (computer === "piedra" && user === "papel") ||
+              (computer === "tijera" && user === "piedra") ||
+              (computer === "papel" && user === "tijera")) {
+        console.log (`You win!! ${user} beats ${computer}.`)
+    }else if ((computer === "papel" && user === "piedra") ||
+              (computer === "piedra" && user === "tijera") ||
+              (computer === "tijera" && user === "papel")) {
+        console.log (`You lose!! ${computer} beats ${user}.`)
+    }else {
+        console.log ("Empataronnnn")
+    }
+}
+
+let seleccionUsuario = prompt ("elige piedra, papel o tijera")
+
+let seleccionComputadora = computerPlay()
+
+jugada(seleccionComputadora, seleccionUsuario);
+
+
 

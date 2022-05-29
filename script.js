ffbb80ca7a62f78d96bbcd5be3527826b63e9127
Juan Lavigna <juanlavigna@gmail.com>
2022-05-29 18:18:43 -0300

    Agrega contador de puntos y resultado final

--- script.js
+++ script.js
@@ -5,27 +5,41 @@ function computerPlay (){
     return computerChoice
 } 
 
+let puntaje = 0
+
 function singleRound (computer, user) {
     if (      (computer === "piedra" && user === "papel") ||
               (computer === "tijera" && user === "piedra") ||
               (computer === "papel" && user === "tijera")) {
-        return console.log (`Ganaste!! ${user} le gana a ${computer}.`)
+        console.log (`Ganaste!! ${user} le gana a ${computer}.`)
+        puntaje++
+        console.log(puntaje)
     }else if ((computer === "papel" && user === "piedra") ||
               (computer === "piedra" && user === "tijera") ||
               (computer === "tijera" && user === "papel")) {
-        return console.log (`Perdiste!! ${computer} le gana a ${user}.`)
+        console.log (`Perdiste!! ${computer} le gana a ${user}.`);
+        puntaje--
+        console.log(puntaje)
     }else if (computer === user){
-        return console.log (`Empataron! ambos eligieron ${user}`)
+        console.log (`Empataron! ambos eligieron ${user}`)
+        console.log(puntaje)
     }else {
-        return console.log ("Entrada no válida")
+        console.log ("Entrada no válida")
     }
 }
 
 function game () {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 10; i++) {
         let seleccionUsuario = (prompt("elige piedra, papel o tijera")).toLowerCase();
         let seleccionComputadora = computerPlay();
         singleRound(seleccionComputadora, seleccionUsuario);
+    }
+    if (puntaje > 0){
+        console.log("Ganaste el juego!")
+    }else if(puntaje < 0) {
+        console.log("Perdiste el juego :(")
+    }else {
+        console.log("Empataron el juego!")
     }
 }
 

44f67d90ff7ff56f0e199d0f481ab9576bb98d6a
Juan Lavigna <juanlavigna@gmail.com>
2022-07-07 23:31:24 -0300

    Add event listeners for every div

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// DOM nodes
+const ROCK = document.querySelector("#rock");
+const PAPER = document.querySelector("#paper");
+const SISSOR = document.querySelector("#sissor");
+const PARRAFO = document.querySelector("p");
+const H2 = document.querySelector("h2")
+let divs = document.querySelectorAll(".contenedor>div")
+
+// Computer random selection
 let armas = ["piedra", "papel", "tijera"]
 
 function computerPlay (){
@@ -5,32 +14,52 @@ function computerPlay (){
     return computerChoice
 } 
 
+// Event listener
+divs.forEach((div) => {
+    div.addEventListener("click", (e) =>{
+        div.classList.add("transform")
+        singleRound(computerPlay(), e.path[0].id)
+    })
+})
+
+divs.forEach((div) => div.addEventListener("transitionend", function (){
+    div.classList.remove("transform")
+}))
+
+function removeTransition (e){
+    if(e.propertyName !== "transform") return;
+    console.log(propertyName)
+}
+
 let puntaje = 0
 
 function singleRound (computer, user) {
     if (      (computer === "piedra" && user === "papel") ||
               (computer === "tijera" && user === "piedra") ||
               (computer === "papel" && user === "tijera")) {
-        console.log (`Ganaste!! ${user} le gana a ${computer}.`)
+        PARRAFO.textContent = `Ganaste!! ${user} le gana a ${computer}.`
         puntaje++
-        console.log(`Puntaje total: ${puntaje}`)
+        H2.textContent = `Puntaje total: ${puntaje}`
     }else if ((computer === "papel" && user === "piedra") ||
               (computer === "piedra" && user === "tijera") ||
               (computer === "tijera" && user === "papel")) {
-        console.log (`Perdiste!! ${computer} le gana a ${user}.`);
+        PARRAFO.textContent = `Perdiste!! ${computer} le gana a ${user}.`
         puntaje--
-        console.log(`Puntaje total: ${puntaje}`)
+        H2.textContent = `Puntaje total: ${puntaje}`
     }else if (computer === user){
-        console.log (`Empataron! ambos eligieron ${user}`)
-        console.log(`Puntaje total: ${puntaje}`)
+        PARRAFO.textContent = `Empataron! ambos eligieron ${user}`
+        H2.textContent = `Puntaje total: ${puntaje}`
     }else {
         console.log ("Entrada no válida")
     }
+
 }
 
-function game () {
+
+
+function game (e) {
     for (let i = 0; i < 10; i++) {
-        let seleccionUsuario = (prompt("elige piedra, papel o tijera")).toLowerCase();
+        let seleccionUsuario = e.path[0].id
         let seleccionComputadora = computerPlay();
         singleRound(seleccionComputadora, seleccionUsuario);
     }
@@ -43,4 +72,5 @@ function game () {
     }
 }
 
-game ();
+
+
